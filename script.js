34cef027a3ae74f4ccbf5be4a40e2ebca5012cc8
Milind Patil <58673870+milinaire25@users.noreply.github.com>
2023-05-19 21:15:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+/* previous JavaScript code... */
+
+document.getElementById('infoBtn').addEventListener('click', function() {
+    document.getElementById('infoPara').textContent = 'There are about 20,000 types of butterflies in the world. Moths are far more numerous, with 160,000 to 500,000 estimated species.';
+});
